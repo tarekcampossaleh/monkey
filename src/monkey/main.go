10b3c594cb0b5b9ae20332cff46b0acfc60dcc40
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-03-18 15:43:44 -0300

    feat(lexer): add repl

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hi %s, welcome to Monkey\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
 }
